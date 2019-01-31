0c98e814925f1e469b9c87e251b6ad3533e9d814
Chris Spencer <chrisspencer1013@gmail.com>
2019-01-31 11:53:13 -0500

    Create palindrome_int.py

---
+++ palindrome_int.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome_bruteforce(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        #print(x)
+        #print(len(x))
+        #print(x[1::-1])
+        if len(x) == 1:
+            return True
+        if len(x) == 2:
+            if x[0] == x[1]:
+                return True
+            else:
+                return False
+        elif x[0] == x[-1]:
+            return self.isPalindrome(x[1:-1])
+        else:
+            return False
