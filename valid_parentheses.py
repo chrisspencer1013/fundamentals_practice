65accf739725f0322cc3abcaca64ed10aba04f00
Chris Spencer <chrisspencer1013@gmail.com>
2019-01-31 16:52:07 -0500

    Update valid_parentheses.py

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -12,3 +12,21 @@ class Solution:
         else:
             return False
                 
+    def isValid_stack(self, s): #36 ms
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        open_paren = ['(', '{', '[']
+        close_paren = [')', '}', ']']
+                
+        for letter in s:
+            if letter in open_paren:
+                stack.append(letter)
+            elif letter in close_paren:
+                if stack == []:
+                    return False
+                elif open_paren.index(stack.pop()) != close_paren.index(letter): #should be same, otherwise invalid
+                    return False
+        return True if stack == [] else False
