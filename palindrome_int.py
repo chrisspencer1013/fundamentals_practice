f9892d13744f148cb0e9480ec72c3020160d0d8b
Chris Spencer <chrisspencer1013@gmail.com>
2019-01-31 12:02:24 -0500

    Update palindrome_int.py

--- palindrome_int.py
+++ palindrome_int.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isPalindrome_bruteforce_recursion(self, x):
+    def isPalindrome_firsttry_recursion(self, x): # 336 ms
         """
         :type x: int
         :rtype: bool
@@ -17,5 +17,23 @@ class Solution:
                 return False
         elif x[0] == x[-1]:
             return self.isPalindrome(x[1:-1])
+        else:
+            return False
+    def isPalindrome_slightly_better_recursion(self, x): # 252 ms
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x_str = str(x)
+        x_len = len(x_str)
+        if x_len == 1:
+            return True
+        elif x_len == 2:
+            if x_str[0] == x_str[1]:
+                return True
+            else:
+                return False
+        elif x_str[0] == x_str[-1]:
+            return self.isPalindrome(x_str[1:-1])
         else:
             return False
